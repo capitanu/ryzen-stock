7a6fb5efe64b91d79881970e893fad216760fcd6
Calin Capitanu <calincapitanu@gmail.com>
2020-11-20 02:33:12 +0100

    curling the sites

---
+++ src/data.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+
+pub struct Sites {
+    pub sites: HashMap<String,String>,
+}
+
+impl Sites {
+    pub fn init() -> Sites {
+	let mut map = HashMap::new();
+	map.insert(
+	    "komplett".to_string(),
+	    "https://www.komplett.se/product/1172141/datorutrustning/datorkomponenter/processor/amd-ryzen-9-5950x-processor".to_string(),
+	);
+	map.insert(
+	    "inet".to_string(),
+	    "https://www.inet.se/produkt/5303477/amd-ryzen-9-5950x-3-4-ghz-72mb".to_string(),
+	);
+	map.insert(
+	    "webhallen".to_string(),
+	    "https://www.webhallen.com/se/product/326491-AMD-Ryzen-9-5950X-16-cores-32-threads-4-9-GHz".to_string(),
+	);
+	map.insert(
+	    "proshop".to_string(),
+	    "https://www.proshop.se/CPU/AMD-Ryzen-9-5950X-CPU-16-kaernor-34-GHz-AMD-AM4-AMD-Boxed-WOF-utan-kylare/2884175".to_string(),
+	);
+	map.insert(
+	    "elgiganten".to_string(),
+	    "https://www.elgiganten.se/product/datorer-tillbehor/datorkomponenter/processor-cpu/228244/amd-ryzen-9-5950x-processor-box".to_string(),
+	);
+
+	Sites{sites: map}
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,102 @@
+use structopt::StructOpt;
+use fern::colors::{Color, ColoredLevelConfig};
+use log::{error};
+
+use ryzen::fetcher;
+use ryzen::data;
+
+
+#[derive(StructOpt, Debug, Clone)]
+#[structopt(
+    name = "ryzen",
+    setting = structopt::clap::AppSettings::ColoredHelp,
+    about = " "
+)]
+
+struct Ryzen {
+    #[structopt(short = "v", long = "verbose", help = "Turn on verbosity")]
+    verbose: bool,
+
+    #[structopt(subcommand)]
+    cmd: Goal,
+}
+
+impl Ryzen {
+    fn run(self){
+	self.logging();
+	match self.cmd.run(){
+	    Ok(()) => (),
+	    Err(err) => error!("{:?}", &err),
+	}
+    }
+
+    fn logging(&self){
+	let colors_line = ColoredLevelConfig::new()
+	    .error(Color::Red)
+            .warn(Color::Yellow)
+            .info(Color::White)
+            .debug(Color::BrightBlack)
+            .trace(Color::BrightBlack);
+	let colors_level = colors_line.clone().info(Color::Green);
+	fern::Dispatch::new()
+	    .format(move |out, message, record| {
+                out.finish(format_args!(
+                    "{color_line}{date} {level}{color_line} :: {message}\x1B[0m",
+                    color_line = format_args!(
+                        "\x1B[{}m",
+                        colors_line.get_color(&record.level()).to_fg_str()
+                    ),
+                    date = chrono::Local::now().format("%H:%M:%S"),
+                    level = colors_level.color(record.level()),
+                    message = message,
+                ));
+            })
+            .level(match self.verbose {
+                true => log::LevelFilter::Debug,
+                false => log::LevelFilter::Info,
+            })
+            .level_for("pretty_colored", log::LevelFilter::Trace)
+            .chain(std::io::stderr())
+            .apply()
+            .unwrap();
+    }
+}
+
+#[derive(StructOpt, Debug, Clone)]
+enum Goal {
+    Checkall(Choice),
+}
+
+impl Goal {
+    fn run(self) -> Result<(), anyhow::Error> {
+        match self {
+            Goal::Checkall(choice) => choice.checkall(),
+        }
+    }
+}
+
+#[derive(StructOpt, Debug, Clone)]
+#[structopt(
+    name = "checkall",
+    about = "Check all the available websites for availability",
+    help = "Takes no other input, just checks all the sites."
+)]
+
+
+struct Choice {
+}
+
+impl Choice {
+    fn checkall(self) -> Result<(), anyhow::Error> {
+	let mut web_struct = data::Sites::init();
+	let mut sites = fetcher::run(web_struct);	
+	println!("{:?}", sites.sites.get("inet"));
+	println!("{:?}", sites.sites.get("komplett"));
+        Ok(())
+    }
+}
+
+
+fn main() {
+    Ryzen::from_args().run(); 
+}
