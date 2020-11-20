77d2b057f9bfbfd4ef5847c8ada47924faf93e3d
Calin Capitanu <calincapitanu@gmail.com>
2020-11-20 13:14:25 +0100

    update

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -2,26 +2,24 @@ extern crate curl;
 
 use std::io::{stdout, Write};
 use curl::easy::Easy;
-
+use std::str;
 
 use crate::data;
 
 pub fn run(mut web_struct: data::Sites) -> data::Sites {
     let all = ["inet", "komplett", "webhallen", "proshop", "elgiganten"];
     for &site in &all {
-	match web_struct.sites.get(site).cloned() {
+	match web_struct.sites.get_mut(site).cloned() {
 	    Some(ref mut x) => {let mut easy = Easy::new();
 			easy.url(x).unwrap();
-			easy.write_function(|data| {
-			    stdout().write_all(data).unwrap();
+			easy.write_function(move |data| {
+			    web_struct.sites.remove(site);
+			    web_struct.sites.insert(x.to_string(),str::from_utf8(data).unwrap().to_string());
 			    Ok(data.len())
 			}).unwrap();
 			easy.perform().unwrap();
-			web_struct.sites.remove(site);
-			web_struct.sites.insert(x.to_string(), easy.response_code().unwrap().to_string());
 	    },
-	    None => {},
-	    
+	    None => (),
 	}
     }
     web_struct
